ffcfd3293b8adffdb61666d06dbd59ec2f392c41
sayf-eddin <sayf.eddin.ahmed@gmail.com>
2023-05-17 12:05:30 -0400

    updated spacing

--- docs/js/site.js
+++ docs/js/site.js
@@ -4,8 +4,7 @@ function navScroll(section) {
 }
 
 var elems = document.querySelectorAll('.carousel');
-for (var i=0, len = elems.length; i < len; i++) 
-{
+for (var i=0, len = elems.length; i < len; i++) {
   var elem = elems[i];
   var flkty = new Flickity( elem, {
     cellAlign: 'left',
@@ -14,10 +13,10 @@ for (var i=0, len = elems.length; i < len; i++)
     autoPlay: 7000,
     dragThreshold: 10,
     arrowShape: { 
-        x0: 10,
-        x1: 60, y1: 50,
-        x2: 60, y2: 50,
-        x3: 60
+      x0: 10,
+      x1: 60, y1: 50,
+      x2: 60, y2: 50,
+      x3: 60
     }
   });
 }
